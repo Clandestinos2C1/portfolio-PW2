52a06a24585ad9032aca147f24dab83918206c5f
luigi <pozzani.luigi@gmail.com>
2026-03-27 10:44:32 -0300

    Added index.php for number interval check (missing code.php)

---
+++ ACT-001-PHP/luigi-nicolasCamargo/ex01/index.php
@@ -0,0 +1,25 @@
+<!--
+Data: 13/03/2026
+Autor: Luigi Pozzani de Souza; Nicolas Camargo Costa Ceccato
+Objetivo:
+
+Exercício 1 - Intervalo entre 100 e 200
+Faça um programa que receba um número e diga se este número está no intervalo entre 100 e 200.
+-->
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 1</title>
+</head>
+<body>
+    <h1> Descubra se seu número está entre 100 e 200 </h1>
+<form action="code.php" method="GET">
+    <label>Insira o Número:</label>
+    <input type="number" name="N1" required>
+
+    <button type="submit">Está entre 100 e 200?</button>
+</form>
+
+</body>
+</html>
