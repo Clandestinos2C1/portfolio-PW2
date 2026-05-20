e4cf679328da8a21ed31be569ee933e4c5f49ace
Murilo Dias <murilo.business.dds@gmail.com>
2026-05-20 18:29:24 -0300

    Adiciona os componetes header e footer

---
+++ 2BIM/act-php-001-crud-pdo/includes/footer.php
@@ -0,0 +1,8 @@
+</div>
+
+<footer>
+    <p>©️ 2026 - Todos os direitos reservados - Pogger's</p>
+</footer>
+
+</body>
+</html>
--- 2BIM/act-php-001-crud-pdo/includes/header.php
+++ 2BIM/act-php-001-crud-pdo/includes/header.php
@@ -1,11 +1,21 @@
-<!doctype html>
-
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pogger's</title>
 
-    <title>Farmácia Clandestina</title>
-    <h2> farmácia clandestina  </h2>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
 
-    </head>
-    <body>
+<header>
+    <h1>Sistema da Farmácia Pogger's</h1>
+
+    <nav>
+        <a href="index.php">Listagem</a>
+        <a href="cadastro.php">Cadastrar</a>
+    </nav>
+</header>
+
+<div class="container">
