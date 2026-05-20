ef3be3d4ebecb1f071db8b35815c145686e5dc82
luigi <pozzani.luigi@gmail.com>
2026-05-19 21:56:28 -0300

    esqueleto do header para teste

---
+++ 2BIM/act-php-001-crud-pdo/includes/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Farmácia Clandestina</title>
+    <h2> farmácia clandestina  </h2>
+
+    </head>
+    <body>
