1ea801f7751c17445128cebca2a816481635b944
Murilo Dias <murilo.business.dds@gmail.com>
2026-06-08 18:44:54 -0300

    Altera insert do banco de dados, adicionando imagens

--- 2BIM/act-php-002-crud-pdo-site/website/banco/tmaqd.sql
+++ 2BIM/act-php-002-crud-pdo-site/website/banco/tmaqd.sql
@@ -51,68 +51,144 @@ INSERT INTO discos
 (titulo, artista, preco, categoria_id, descricao, destaque, estoque, imagem)
 VALUES
 
-(
-'The Dark Side of the Room',
-'Echo Chambers',
-89.90,
-4,
-'Álbum indie atmosférico e melancólico.',
-0,
-12,
-'darkside.jpg'
-),
+INSERT INTO discos
+(titulo, artista, preco, categoria_id, descricao, destaque, estoque, imagem)
+VALUES
 
+-- ROCK (1)
 (
-'Neon Hearts',
-'Luna Waves',
-119.90,
-2,
-'Pop moderno com sintetizadores vibrantes e refrões marcantes.',
-0,
-8,
-'neonhearts.jpg'
-),
-
-(
-'Noites de Bossa',
-'Clara Mendes',
-97.50,
-3,
-'Uma homenagem moderna à bossa nova.',
+'The Dark Side of the Moon',
+'Pink Floyd',
+129.90,
+1,
+'Um dos álbuns mais influentes da história do rock.',
 1,
 15,
-'bossa.jpg'
+'https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f'
 ),
 
 (
-'Seoul Dreams',
-'NovaPulse',
-129.90,
-6,
-'K-pop energético com influências eletrônicas e visuais futuristas.',
-0,
-20,
-'seouldreams.jpg'
-),
-
-(
-'Chuva de Flores',
-'Orquestra Paulista',
-135.00,
-5,
-'Coleção instrumental clássica brasileira.',
-0,
-5,
-'chuva.jpg'
-),
-
-(
-'Radio Nowhere',
-'The Static Fields',
-88.00,
+'Back in Black',
+'AC/DC',
+119.90,
 1,
-'Rock alternativo energético e nostálgico.',
+'Clássico absoluto do hard rock.',
 0,
 10,
-'radionowhere.jpg'
+'https://images.unsplash.com/photo-1511379938547-c1f69419868d'
+),
+
+-- POP (2)
+(
+'SOUR',
+'Olivia Rodrigo',
+99.90,
+2,
+'Álbum de estreia que conquistou milhões de fãs.',
+1,
+20,
+'https://images.unsplash.com/photo-1516280440614-37939bbacd81'
+),
+
+(
+'GUTS',
+'Olivia Rodrigo',
+109.90,
+2,
+'Sucesso mundial com hits e letras marcantes.',
+1,
+18,
+'https://images.unsplash.com/photo-1501612780327-45045538702b'
+),
+
+-- MPB (3)
+(
+'Clube da Esquina',
+'Milton Nascimento & Lô Borges',
+89.90,
+3,
+'Uma das maiores obras da música brasileira.',
+1,
+12,
+'https://images.unsplash.com/photo-1487180144351-b8472da7d491'
+),
+
+(
+'Construção',
+'Chico Buarque',
+84.90,
+3,
+'Álbum histórico da MPB.',
+0,
+8,
+'https://images.unsplash.com/photo-1510915361894-db8b60106cb1'
+),
+
+-- INDIE (4)
+(
+'AM',
+'Arctic Monkeys',
+114.90,
+4,
+'Referência moderna do indie rock.',
+1,
+16,
+'https://images.unsplash.com/photo-1514320291840-2e0a9bf2a9ae'
+),
+
+(
+'Currents',
+'Tame Impala',
+119.90,
+4,
+'Psicodelia moderna com produção impecável.',
+1,
+11,
+'https://images.unsplash.com/photo-1507838153414-b4b713384a76'
+),
+
+-- CLÁSSICOS (5)
+(
+'The Four Seasons',
+'Antonio Vivaldi',
+74.90,
+5,
+'Uma das composições mais famosas da música clássica.',
+0,
+9,
+'https://images.unsplash.com/photo-1465847899084-d164df4dedc6'
+),
+
+(
+'Symphony No. 5',
+'Ludwig van Beethoven',
+79.90,
+5,
+'Obra-prima da música erudita.',
+1,
+7,
+'https://images.unsplash.com/photo-1507838153414-b4b713384a76'
+),
+
+-- K-POP (6)
+(
+'MAP OF THE SOUL: 7',
+'BTS',
+134.90,
+6,
+'Um dos álbuns mais vendidos do grupo.',
+1,
+25,
+'https://images.unsplash.com/photo-1501386761578-eac5c94b800a'
+),
+
+(
+'BORN PINK',
+'BLACKPINK',
+129.90,
+6,
+'Grande sucesso do quarteto sul-coreano.',
+1,
+22,
+'https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f'
 );
